dfdba4475a79e118193b062d929b9b6633930299
keyurparalkar <keur.plkar@gmail.com>
2024-01-22 16:45:54 +0530

    feat: added context to the time display component

--- src/components/TimeDisplay.tsx
+++ src/components/TimeDisplay.tsx
@@ -1,4 +1,10 @@
+import { useContext } from 'react';
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
 import styled from 'styled-components';
+import { PlayerContext } from '../context';
+
+dayjs.extend(duration);
 
 const StyledContainer = styled.span`
     margin: 0.4rem 1rem 0.7rem 0rem;
@@ -7,11 +13,16 @@ const StyledContainer = styled.span`
 `;
 
 const TimeDisplay = () => {
+    const { currentTime, totalDuration } = useContext(PlayerContext);
+
+    const formattedCurrentTime = dayjs.duration(currentTime, 'seconds').format('m:ss') || '0:00';
+    const formattedTotalDuration = dayjs.duration(totalDuration, 'seconds').format('m:ss') || '0:00';
+
     return (
         <StyledContainer className="time-display-control">
-            <span className="control-current-time">0:51</span>
+            <span className="control-current-time">{formattedCurrentTime}</span>
             <span className="control-separator"> / </span>
-            <span className="control-totaltime">1:10</span>
+            <span className="control-totaltime">{formattedTotalDuration}</span>
         </StyledContainer>
     );
 };
