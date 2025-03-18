3e78305025b6f44a9ec1f8e30d67772358e4ac3f
keyurparalkar <keur.plkar@gmail.com>
2025-03-18 21:42:44 +0530

    feat: added sentry for testing

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { PlayerProvider } from './context';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { PlayerProvider } from "./context";
+import * as Sentry from "@sentry/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+Sentry.init({
+	dsn: "https://27373f63518870e27311430b25ad64ae@o846950.ingest.us.sentry.io/4508999610335232",
+});
+
+const root = ReactDOM.createRoot(
+	document.getElementById("root") as HTMLElement
+);
 root.render(
-    <React.StrictMode>
-        <PlayerProvider>
-            <App />
-        </PlayerProvider>
-    </React.StrictMode>,
+	<React.StrictMode>
+		<PlayerProvider>
+			<App />
+		</PlayerProvider>
+	</React.StrictMode>
 );
